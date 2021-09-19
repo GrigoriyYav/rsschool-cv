77921ccb9af3f0c9ac772a2d90f9e33065efb6ed
Grigorii Iavorskii <yavgan06@gmail.com>
2021-09-19 12:55:35 +0400

    feat: add function for auto-close menu

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 // nav
 const iconMenu = document.querySelector('.menu_icon');
+const menuBody= document.querySelector('.page-header-nav');
 if (iconMenu){
-    const menuBody= document.querySelector('.page-header-nav');
-    iconMenu.addEventListener("click", function (e) {
+        iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     });
+}
+
+const menu = document.querySelectorAll('.site-navigation__link');
+if(menu.length > 0){
+    menu.forEach(menu =>{
+        menu.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        if(iconMenu.classList.contains('_active')){
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+        }
+    }
 }
